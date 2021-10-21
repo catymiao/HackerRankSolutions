a3b260e7f051190f2bec9a48d25e96e267d230ed
catymiao <33770231+catymiao@users.noreply.github.com>
2021-10-21 10:14:05 -0500

    Create What's your name.py

---
+++ What's your name.py
@@ -0,0 +1,15 @@
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+
+
+def print_full_name(first, last):
+    print("Hello %s %s! You just delved into python."%(first,last))
+    # use %d when the variable is a number 
+    
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
