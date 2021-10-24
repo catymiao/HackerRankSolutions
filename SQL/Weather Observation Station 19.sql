b1c20103f70ea28b3845b685870798b7ec9d5a3c
catymiao <33770231+catymiao@users.noreply.github.com>
2021-10-24 11:20:44 -0500

    Update Weather Observation Station 19.sql

--- SQL/Weather Observation Station 19.sql
+++ SQL/Weather Observation Station 19.sql
@@ -1,7 +1,9 @@
+/* Euclidean Distance between ()
+
 /*
 MySQL Syntax
 */
-SELECT Round(SQRT(power(max(lat_n)-MIN(lat_N),2) + power(MAX(long_w)-MIN(Long_w),2)),4)  FROM STATION 
+SELECT Round(SQRT(power(max(lat_n)-MIN(lat_N),2) + power(MAX(long_w)-MIN(Long_w),2)),4) FROM STATION 
 
 
 
