b44a1bdbfc1020d7445ec9d212f486dcce60004d
catymiao <33770231+catymiao@users.noreply.github.com>
2021-10-24 10:49:53 -0500

    Create Placements.sql

---
+++ SQL/Placements.sql
@@ -0,0 +1,14 @@
+/*
+MySQL
+*/
+select Name from (
+SELECT s.Name, p1.salary AS StudentSalary, p2.Salary AS FriendSalary
+FROM Students s
+INNER JOIN Friends f
+on s.ID= f.ID
+INNER JOIN Packages p1
+on s.ID = P1.ID 
+INNER JOIN Packages p2
+on f.Friend_ID = p2.ID)a 
+Where FriendSalary > StudentSalary
+ORDER BY FriendSalary
