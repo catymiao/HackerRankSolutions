6bfb47ec8003070113c7bf7a56e6e84258571abc
catymiao <33770231+catymiao@users.noreply.github.com>
2021-10-25 22:38:33 -0500

    Update Weather Observation Station 20.sql

--- SQL/Weather Observation Station 20.sql
+++ SQL/Weather Observation Station 20.sql
@@ -2,7 +2,7 @@
 Enter your MS SQL Server query here.
 */
 
-select cast(round(lat_n,4) as numeric(18,4)) from (
+SELECT cast(round(lat_n,4) as numeric(18,4)) FROM (
 SELECT lat_n, RANK() OVER (order by LAT_N desc) as RANK FROM STATION 
 )a
 where rank = (SELECT CASE WHEN count(*)%2 != 0 THEN (count(*)+1)*0.5
