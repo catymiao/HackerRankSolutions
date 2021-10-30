5219450f221f3168abb37fa719cb297a9016a558
catymiao <33770231+catymiao@users.noreply.github.com>
2021-10-30 15:22:04 -0500

    Update itertools.product().py

--- itertools.product().py
+++ itertools.product().py
@@ -6,4 +6,4 @@ storage2 = set(input().split());
 storage2=sorted(list(map(int,storage2)))
 #print(storage2)
 for i in list(itertools.product(storage1,storage2)):
-    print(i,end=" ") 
+    print(i,end=" ") #print in one line
