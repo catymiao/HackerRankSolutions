d068957a7fdfc559cdc127583816f2a8d96f17ab
catymiao <33770231+catymiao@users.noreply.github.com>
2021-10-30 15:19:22 -0500

    Create itertools.product().py

---
+++ itertools.product().py
@@ -0,0 +1,9 @@
+import itertools
+storage1 = set(input().split());
+storage1=sorted(list(map(int,storage1)))
+#print(storage1)
+storage2 = set(input().split());
+storage2=sorted(list(map(int,storage2)))
+#print(storage2)
+for i in list(itertools.product(storage1,storage2)):
+    print(i,end=" ") 
