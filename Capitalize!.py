a70fd9044dfa2d0b17e579b03d96c1a24c45058b
catymiao <33770231+catymiao@users.noreply.github.com>
2021-10-30 10:31:51 -0500

    Create Capitalize!.py

---
+++ Capitalize!.py
@@ -0,0 +1,11 @@
+def solve(s):
+    l=s.split('\\b')
+    string = ''.join(l)
+    new_str = ''
+    
+    for i in range(len(string)):
+        if i == 0 or string[i-1] ==' ':
+            new_str += string[i].upper()
+        else:
+            new_str += string[i]
+    return(new_str)
