9d0898fc9e60f44a7d337d68ab8224d08a589664
catymiao <33770231+catymiao@users.noreply.github.com>
2021-11-15 20:23:01 -0600

    Update String Validators.py

--- String Validators.py
+++ String Validators.py
@@ -1,5 +1,6 @@
 if __name__ == '__main__':
-    s = input()
+    s = raw_input()
+
     n1=0
     n2=0
     n3=0
@@ -9,39 +10,33 @@ if __name__ == '__main__':
     for i in s:
         if i.isalnum()==True:
             n1+=1
+        if i.isalpha()==True:
+            n2+=1
+        if i.isdigit()==True:
+            n3+=1
+        if i.islower()==True:
+            n4+=1
+        if i.isupper()==True:
+            n5+=1
     if n1>= 1:
         print(True)
     else:
         print(False)
-        
-    for i in s:
-        if i.isalpha()==True:
-            n2+=1
     if n2>= 1:
         print(True)
     else:
-        print(False)    
-        
-    for i in s:
-        if i.isdigit()==True:
-            n3+=1
+        print(False) 
     if n3>= 1:
         print(True)
     else:
-        print(False)    
-        
-    for i in s:
-        if i.islower()==True:
-            n4+=1
+        print(False) 
     if n4>= 1:
         print(True)
     else:
-        print(False)   
-        
-    for i in s:
-        if i.isupper()==True:
-            n5+=1
+        print(False) 
     if n5>= 1:
         print(True)
     else:
         print(False)
+        
+        
